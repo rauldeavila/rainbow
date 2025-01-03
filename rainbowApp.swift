37d66d0e2c9c08fcabdc6a99077c0281e8572b7e
Raul (bwulf) <rauldeavila93@gmail.com>
2025-01-03 10:09:24 -0300

    hello-world

---
+++ rainbowApp.swift
@@ -0,0 +1,17 @@
+//
+//  rainbowApp.swift
+//  rainbow
+//
+//  Created by Raul de Avila Junior on 03/01/25.
+//
+
+import SwiftUI
+
+@main
+struct rainbowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
